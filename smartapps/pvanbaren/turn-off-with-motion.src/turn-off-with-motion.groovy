ddc13d05c6812721f08ea7639acb7d88406b860c
Philip Van Baren <pdvb_smart@vibtest.com>
2016-03-31 08:00:08 -0400

    Make it turn the switch OFF, not on

--- smartapps/pvanbaren/turn-off-with-motion.src/turn-off-with-motion.groovy
+++ smartapps/pvanbaren/turn-off-with-motion.src/turn-off-with-motion.groovy
@@ -1,8 +1,7 @@
 /**
  *  Turn Off With Motion
  *
- *	Turn off one or more switches as soon as movement is detected,
- *  and then turn them back on after some amount of time has passed without movement.
+ *	Turn off one or more switches as soon as movement is detected.
  *
  *  Copyright 2016 Philip Van Baren
  *
@@ -20,31 +19,26 @@ definition(
 	name: "Turn Off With Motion",
 	namespace: "pvanbaren",
 	author: "Philip Van Baren",
-	description: "Turns off a switch when motion occurs, turns it back on after a timer expires",
+	description: "Turns off a switch when motion occurs",
 	category: "My Apps",
 	iconUrl: "https://s3.amazonaws.com/smartapp-icons/Solution/switches.png",
 	iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Solution/switches@2x.png")
 
 preferences
 {
-	section("Turn off switch(es)...")
-	{
-		input "theswitch", "capability.switch", multiple: true
-	}
-	section("when there is movement...")
+	section("When there is movement...")
 	{
 		input "themotion", "capability.motionSensor", title: "Where?", multiple: true
 	}
-	section("Turn back on after movement ceases for...")
+	section("Turn off switch(es)...")
 	{
-		input "thetime", "number", title: "Minutes?"
+		input "theswitch", "capability.switch", title: "Which?", multiple: true
 	}
 }
 
 def installed()
 {
 	subscribe(themotion, "motion.active", motionDetectedHandler)
-	subscribe(themotion, "motion.inactive", motionStoppedHandler)
 }
 
 def updated()
@@ -59,58 +53,4 @@ def motionDetectedHandler(evt)
 	log.debug "Turning off the switch"
 
 	theswitch.off()
-	resetTimeout()
-}
-
-def motionStoppedHandler(evt)
-{
-	log.debug "$evt.name: $evt.value"
-	log.debug "setting timeout"
-	resetTimeout()
-}
-
-def checkMotion()
-{
-	def motionState = themotion.currentState("motion")
-
-	// Check if any motion sensor is active
-	if (motionState.contains("active"))
-	{
-		// Motion active; just log it and do nothing
-		log.debug "Motion is active yet, resetting the timer"
-		resetTimeout()
-	}
-	else
-	{
-		// Get the time elapsed between now and when the motion reported inactive
-		// The elapsed time is in milliseconds, but comparisons are in seconds
-		def elapsed = (now() - state.lastMotion) / 1000
-
-		def threshold = 60 * thetime
-
-		if (elapsed >= (threshold - 1))
-		{
-			log.debug "Motion inactive for $elapsed sec:  turning switch on"
-			theswitch.on()
-		}
-		else
-		{
-			log.debug "Motion inactive for $elapsed sec"
-			scheduleCheckMotion(threshold - elapsed)
-		}
-	}
-}
-
-private resetTimeout()
-{
-	state.lastMotion = now()
-	scheduleCheckMotion(60 * thetime)
-}
-
-private scheduleCheckMotion(def inSeconds)
-{
-	// Try to turn off the lights after the timeout
-	// but no sooner than 30 seconds from now
-	def nextCheck = Math.max(30, inSeconds)
-	runIn(nextCheck, checkMotion)
 }
